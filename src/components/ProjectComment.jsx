11f8038ce7cd6a10c854af984aba7a0bcc2409ce
Akhror <akhsoliev@gmail.com>
2024-12-29 11:43:25 +0500

    implement ProjectComment and ProjectSummary components; enhance Project page with document fetching and error handling; update ProjectList links and MainLayout styles

--- src/components/ProjectComment.jsx
+++ src/components/ProjectComment.jsx
@@ -1,36 +1,50 @@
 function ProjectComment() {
-  const projects = [
-    {
-      name: "Project 1",
-      id: 1,
-      category: "development",
-      createdAt: "Fri Dec 13 2024",
-      details: "lorem ipsum dolor",
-      dueDate: "",
-      createdBy: {
-        id: "created user id",
-        displayName: "User 1",
-        photoURL: "photoURL",
-      },
-      comments: [
-        {
-          id: 1,
-          createdAt: "Fri Dec 20 2024",
-          content: "Comment text 1",
-          photoURL: "photoURL",
-        },
-      ],
-      assignedUsersList: [
-        {
-          id: 1,
-          displayName: "Test User 1",
-          photoURL: "photoURL",
-        },
-      ],
-    },
-  ];
+  return (
+    <div className="py-5">
+      <h4 className="mb-4 text-xl font-medium">Project Comments:</h4>
 
-  return <div>ProjectComment</div>;
+      <div className="chat chat-start">
+        <div className="avatar chat-image">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          Obi-Wan Kenobi
+          <time className="text-xs opacity-50">12:45</time>
+        </div>
+        <div className="chat-bubble">You were the Chosen One!</div>
+        <div className="chat-footer opacity-50">Delivered</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="avatar chat-image">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          Anakin
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
+        <div className="chat-bubble">I hate you!</div>
+        <div className="chat-footer opacity-50">Seen at 12:46</div>
+      </div>
+
+      <div>
+        <p className="mb-2 text-sm">Add new comment:</p>
+        <textarea
+          className="textarea textarea-bordered w-full"
+          placeholder="Type your comment here..."
+        ></textarea>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectComment;
