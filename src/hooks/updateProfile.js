17d5a7a39e9f67a2254f04bd10f916d896966c9d
Akhror <akhsoliev@gmail.com>
2025-01-01 23:25:01 +0500

    add User page and routing; implement user profile display; enhance user data handling in hooks; update OnlineUsers component for user links; refactor profile update logic

--- src/hooks/updateProfile.js
+++ src/hooks/updateProfile.js
@@ -14,36 +14,70 @@ export function useUpdateUser() {
   const [isCancelled, setIsCancelled] = useState(false);
   const [isPending, setIsPending] = useState(false);
 
-  const updateUserProfile = async (displayName = false, imageForm = false) => {
+  const updateUserProfile = async (
+    displayName = false,
+    avatarImage = false,
+    coverImage = false,
+  ) => {
     setIsPending(true);
+
     const updateData = {};
+
+    let _avatarImage;
+    let _coverImage;
+
     try {
-      if (imageForm) {
-        const formData = new FormData(imageForm);
-        const res = await axios.post(imageBase, formData, {
+      if (avatarImage) {
+        const formData1 = new FormData();
+        formData1.append("file", avatarImage);
+        const res1 = await axios.post(imageBase, formData1, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
         });
-        updateData.photoURL = res.data;
+
+        updateData.photoURL = res1.data;
+        _avatarImage = res1.data;
+      }
+
+      if (coverImage) {
+        const formData2 = new FormData();
+        formData2.append("file", coverImage);
+        const res2 = await axios.post(imageBase, formData2, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        _coverImage = res2.data;
       }
 
       if (displayName && user.displayName !== displayName) {
         updateData.displayName = displayName;
       }
 
+      if (_coverImage) {
+        await updateDocument(user.uid, {
+          coverURL: _coverImage,
+        });
+      }
+
       if (Object.keys(updateData).length > 0 && !isCancelled) {
         await updateProfile(auth.currentUser, updateData);
         await updateDocument(user.uid, {
-          id: auth.currentUser.uid,
           displayName: auth.currentUser.displayName,
           photoURL: auth.currentUser.photoURL,
           online: true,
+          email: auth.currentUser.email,
+          emailVerified: auth.currentUser.emailVerified,
+          createdAt: auth.currentUser.metadata.createdAt,
         });
         dispatch({ type: "LOGIN", payload: { ...user, ...updateData } });
-        toast.success("Profile updated successfully");
+      }
+
+      if (_coverImage && Object.keys(updateData).length > 0 && !isCancelled) {
+        toast.error("No any changes made");
       } else {
-        toast.error("No changes to update");
+        toast.success("Profile updated successfully");
       }
     } catch (error) {
       console.error("Profile update error:", error.message);
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Login } from "./Login";
 export { default as Project } from "./Project";
 export { default as Signup } from "./Signup";
 export { default as Profile } from "./Profile";
+export { default as User } from "./User";
